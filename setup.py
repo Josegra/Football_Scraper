f934886d9bb8db0f1cfdc4803ebd47ec788d9b86
Jose Gonzalez <josegra1163@gmail.com>
2025-01-07 00:45:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='football_scraper_jose',  # Replace with your desired package name
     version='0.1',  # Start with version 0.1 or any version you'd like
-    py_modules=['Fbref'],  # Name of the .py file (without the extension)
+    py_modules=['stats_merger'],  # Name of the .py file (without the extension)
     install_requires=[  # List any external libraries your file depends on
         # For example, if your script needs numpy, add it here:
         # 'numpy',
